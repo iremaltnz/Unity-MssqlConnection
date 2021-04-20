ae7b25cc3e9ebc72df90b839dcddb2d4323c891f
iremaltnz <55363753+iremaltnz@users.noreply.github.com>
2021-04-21 02:40:15 +0300

    MssqlConnection

---
+++ Assets/codes/GamerRegister.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamerRegister : MonoBehaviour
+{
+    public GameObject UserName;
+    public GameObject Password;
+    public Text RegisterMessage;
+
+    private string userName;
+    private string password;
+
+    public void Register()
+    {
+        userName = UserName.GetComponent<InputField>().text.ToString();
+        password = Password.GetComponent<InputField>().text.ToString();
+
+        MssqlConnection mssqlConnection = new MssqlConnection();
+        mssqlConnection.GamerRegister(userName,password);
+
+        bool check= mssqlConnection.GamerLoginCheck(userName,password);
+
+        if (check)
+        {
+            RegisterMessage.text =  "Register Success";
+        }
+        else
+        {
+            RegisterMessage.text = "Register Error";
+        }
+
+    }
+}
---
+++ Assets/codes/MssqlConnection.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System.Data.SqlClient;
+
+public class MssqlConnection : MonoBehaviour
+{
+    public string connect =@"Data Source=IREM-\SQLSERVER; Initial Catalog=Unity-MssqlConnection; User ID=deneme2;Password=13579;Trusted_Connection=False;";
+    void Start()
+    {
+        SqlConnectionCheck();
+    } 
+
+    void SqlConnectionCheck()
+    {
+        SqlConnection sqlConnection = new SqlConnection(connect);
+        try
+        {
+            sqlConnection.Open();
+            Debug.Log("Success Connection");
+         
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Error Connection");
+            throw e;
+        }
+    }
+
+    public void GamerRegister(string UserName, string Password)
+    {
+        SqlConnection sqlConnection = new SqlConnection(connect);
+        sqlConnection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand("INSERT Gamers(UserName,Password) VALUES('" + UserName + "','" + Password + "')", sqlConnection);
+        sqlCommand.ExecuteNonQuery();
+        sqlConnection.Close();
+
+    }
+
+    public bool GamerLoginCheck(string UserName, string Password)
+    {
+        SqlConnection sqlConnection = new SqlConnection(connect); ;
+        sqlConnection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Gamers WHERE UserName='" + UserName + "' and Password='" + Password + "'", sqlConnection);
+        SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+        int sayac = 0;
+
+        while (sqlDataReader.Read())
+        {
+            sayac++;
+  
+        }
+
+        sqlConnection.Close();
+
+        if (sayac == 0)
+        {
+ 
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
+
+    }
+
+}
